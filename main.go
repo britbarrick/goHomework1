ebf5494b169b8b025e67a213edfb80a8301dd891
Brit Barrick <brittneybarrick@gmail.com>
2019-06-20 11:35:29 -0500

    2a: Port Flag Added

--- main.go
+++ main.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
+	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
+
+var dad = []string{
+	"Did you hear the one about the guy with the broken hearing aid? Neither did he.",
+	"What do you call a fly without wings? A walk.",
+	"When my wife told me to stop impersonating a flamingo, I had to put my foot down.",
+	"What do you call someone with no nose? Nobody knows.",
+	"What time did the man go to the dentist? Tooth hurt-y.",
+	"Why can’t you hear a pterodactyl go to the bathroom? The p is silent.",
+	"How many optometrists does it take to change a light bulb? 1 or 2? 1... or 2?",
+	"I was thinking about moving to Moscow but there is no point Russian into things.",
+	"Why does Waldo only wear stripes? Because he doesn't want to be spotted.",
+	"Do you know where you can get chicken broth in bulk? The stock market.",
+}
 
 // Returns Pong
 func getPong(w http.ResponseWriter, r *http.Request) {
@@ -15,36 +31,18 @@ func getPong(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("pong")
 }
 
-// Random Dad Joke Switcher
+// Random Dad Joke Switcher -- TODO: place http req to canhazdadjoke
 func getJoke(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var dj string
-	switch rand.Intn(10) {
-	case 0:
-		dj = "Did you hear the one about the guy with the broken hearing aid? Neither did he."
-	case 1:
-		dj = "What do you call a fly without wings? A walk."
-	case 2:
-		dj = "When my wife told me to stop impersonating a flamingo, I had to put my foot down."
-	case 3:
-		dj = "What do you call someone with no nose? Nobody knows."
-	case 4:
-		dj = "What time did the man go to the dentist? Tooth hurt-y."
-	case 5:
-		dj = "Why can’t you hear a pterodactyl go to the bathroom? The p is silent."
-	case 6:
-		dj = "How many optometrists does it take to change a light bulb? 1 or 2? 1... or 2?"
-	case 7:
-		dj = "I was thinking about moving to Moscow but there is no point Russian into things."
-	case 8:
-		dj = "Why does Waldo only wear stripes? Because he doesn't want to be spotted."
-	case 9:
-		dj = "Do you know where you can get chicken broth in bulk? The stock market."
-	}
+	dj := dad[rand.Intn(10)]
 	json.NewEncoder(w).Encode(dj)
 }
 
 func main() {
+
+	customPort := flag.Int("port", 5309, "Define custom port")
+	flag.Parse()
+
 	// Init router
 	r := mux.NewRouter()
 
@@ -52,5 +50,12 @@ func main() {
 	r.HandleFunc("/ping", getPong).Methods("GET")
 	r.HandleFunc("/v1/joke", getJoke).Methods("GET")
 
-	log.Fatal(http.ListenAndServe(":5309", r))
+	if *customPort <= 1024 || *customPort >= 65534 {
+		fmt.Println("Invalid port: please enter a value between 1025 and 65533")
+		os.Exit(1)
+	}
+
+	port := fmt.Sprintf(":%v", *customPort)
+	log.Fatal(http.ListenAndServe(port, r))
+
 }
